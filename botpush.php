761c0cddd569b3a6a791b36483eff1cd7d645b31
praewiiezzz <chutikan_praew@hotmail.com>
2018-12-09 15:37:58 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -17,6 +17,7 @@ $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+echo 'ok'
 
 
 
